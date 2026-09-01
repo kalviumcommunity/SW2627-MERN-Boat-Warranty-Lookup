ed29ac268fa9af0b55d85e0aa4560b73c0c1e1ca
piyushrao741 <piyush.yadav.s.124@kalvium.community>
2026-09-01 15:17:51 +0530

    feat: migrate product api to prisma

--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const {
   createProduct,
-  getProductBySerialNumber
+  getProductBySerialNumber,
 } = require("../controllers/product.controller");
 
 const router = express.Router();
